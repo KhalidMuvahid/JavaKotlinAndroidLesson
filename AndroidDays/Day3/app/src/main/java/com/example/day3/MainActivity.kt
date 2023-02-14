7e7f8769e03ea3bdae3986897ed99e0dcc84ca5d
khal <khanifkhalid@gmail.com>
2023-02-14 12:36:55 +0300

    AndroidLesson Day4

--- AndroidDays/Day3/app/src/main/java/com/example/day3/MainActivity.kt
+++ AndroidDays/Day3/app/src/main/java/com/example/day3/MainActivity.kt
@@ -3,12 +3,21 @@ package com.example.day3
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.LayoutInflater
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.CompoundButton
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.ProgressBar
 import android.widget.Toast
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
@@ -16,44 +25,27 @@ import com.google.android.material.textfield.TextInputLayout
 class MainActivity : AppCompatActivity() {
 
 
-    lateinit var  textInputEditText:TextInputEditText;
-
-  /*  val textWatcher = object : MyTextWatcher() {
-
-        override fun afterTextChanged(s: Editable?) {
-            val input = s.toString()
-            if (input.endsWith("@g")){
-                val fullMail = "${input}mail.com"
-                setText("${input}mail.com")
-            }
-            val valid = android.util.Patterns.EMAIL_ADDRESS.matcher(input).matches()
-            textInputLayout.isErrorEnabled = !valid
-            val error = if (valid)"" else getString(R.string.invalid_email_message)
-            textInputLayout.error = error
-            if (valid) Toast.makeText(this@MainActivity,R.string.valid_email_message,Toast.LENGTH_SHORT).show()
-
-        }
-
-        private fun setText(text: String){
-        textInputEditText.removeTextChangedListener(textWatcher)
-        textInputEditText.setTextCorrectly(text)
-        }
-
-         fun TextInputEditText.setTextCorrectly(text:CharSequence){
-        setText(text)
-        setSelection(text.length)
-        }
-
-        textInputEditText.addTextChangedListener(textWatcher)
-
-    }*/
+    lateinit var  textInputEditText:TextInputEditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val textInputLayout = findViewById<TextInputLayout>(R.id.inputTextLayout)
         textInputEditText = findViewById<TextInputEditText>(R.id.textInputEditText)
+        val checkBox = findViewById<CheckBox>(R.id.agree_checkbox)
         val logButton = findViewById<Button>(R.id.log_button)
+        val contentLayout = findViewById<LinearLayout>(R.id.content_layout)
+        val progressBarLayout = findViewById<LinearLayout>(R.id.progress_bar_layout)
+
+
+        logButton.isEnabled =false
+        checkBox.setOnCheckedChangeListener { _, isChecked ->
+            logButton.isEnabled = isChecked
+        }
+
+
+
+
 
         textInputEditText.listenerChanges {
             textInputLayout.isErrorEnabled = false
@@ -63,11 +55,26 @@ class MainActivity : AppCompatActivity() {
             hideKeyboard(textInputEditText)
             if (android.util.Patterns.EMAIL_ADDRESS.matcher(textInputEditText.text.toString()).matches()){
                 Snackbar.make(logButton,"go to postLogin",Snackbar.LENGTH_LONG).show()
+                contentLayout.visibility = View.GONE
+                progressBarLayout.visibility = View.VISIBLE
+                Handler(Looper.myLooper()!!).postDelayed({
+                    contentLayout.visibility = View.VISIBLE
+                    progressBarLayout.visibility = View.GONE
+                    val bottomSheetDialog= BottomSheetDialog(this)
+                    val view = LayoutInflater.from(this).inflate(R.layout.dialog,contentLayout,false)
+                    bottomSheetDialog.setCancelable(false)
+                    view.findViewById<ImageButton>(R.id.cancelBut).setOnClickListener { bottomSheetDialog.dismiss()}
+                    bottomSheetDialog.setContentView(view)
+                    bottomSheetDialog.show()
+                },3000)
             }else{
                 textInputLayout.isErrorEnabled = true
                 textInputLayout.error = getString(R.string.invalid_email_message)
             }
         }
+
+
+
 
 
     }
